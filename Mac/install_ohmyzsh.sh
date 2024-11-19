1c1e550c18d05aad90b0831638f7fa3eeed6ac3b
chuckisbusy <chuckisbusy@icloud.com>
2024-11-19 13:19:13 -0500

    Update install_ohmyzsh.sh

--- Mac/install_ohmyzsh.sh
+++ Mac/install_ohmyzsh.sh
@@ -6,7 +6,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/hlissner/zsh-autopair.git ~/.oh-my-zsh/custom/plugins/zsh-autopair
-git clone https://github.com/zap-zsh/supercharge.git ~/.oh-my-zsh/custom/plugins/supercharge
 git clone https://github.com/wting/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
 
 cd $HOME/.oh-my-zsh/custom/plugins/autojump
